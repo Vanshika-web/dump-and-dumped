5bee5550b64e0342de46c5538d36003b1a4771f7
Vanshika-web <72748218+Vanshika-web@users.noreply.github.com>
2020-11-22 22:54:03 +0530

    Add files via upload

--- paper.js
+++ paper.js
@@ -1,26 +1,39 @@
-class paper {
-    constructor(x,y,r) {
-      var options = {
-          isStatic: false,
-          restitution:0.3,
-          friction:0.5,
-          density:1.2
-      }
-      this.x = x;
-      this.y = y;
-      this.r = r;
-      this.body = Bodies.circle(this.x, this.y, this.r/2, options)
-      World.add(world, this.body);
+class paper
+{
+constructor(x,y,r)
+{
+var options={
+isStatic:false,
+restitution:0.3,
+friction:0,
+density:1.2
 
-    }
-    display(){
-      var pos =this.body.position;
-      push();
-      translate(pos.x, pos.y);
-      rectMode(CENTER);
-      strokeWeight(3);
-      fill(255, 0, 255);
-      ellipse(0,0,this.r,this.r);
-      pop();
-    }
-  };    
+
+}
+this.x=x;
+this.y=y;
+this.r=r
+this.image=loadImage("paper(1).png");
+this.body=Bodies.circle(this.x, this.y, (this.r-20)/2, options)
+World.add(world, this.body);
+
+}
+display()
+{
+
+var paperpos=this.body.position;
+
+push()
+translate(paperpos.x, paperpos.y);
+rectMode(CENTER)
+//strokeWeight(4);
+fill(255,0,255)
+imageMode(CENTER);
+image(this.image, 0,0,this.r, this.r)
+//ellipse(0,0,this.r, this.r);
+pop()
+
+}
+
+}
+
